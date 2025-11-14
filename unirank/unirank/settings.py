bbbae3abc4ae69a8763682f758c248316652d08b
Gourav <132838923+gouravdev246@users.noreply.github.com>
2025-11-14 20:22:54 +0530

    add vercel

--- unirank/unirank/settings.py
+++ unirank/unirank/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-bty%0ka_o2rki9#e5!)dt18=!0l6o&nt2bu#65%w*-wn!4dw$(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+
 
 
 # Application definition
@@ -131,11 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
